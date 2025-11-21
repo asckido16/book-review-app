9c3867f800af58441e895a94b98591a870dd92fd
Aschalew Kidane <asckido@gmail.com>
2025-11-21 20:18:37 +0300

    Weyeee Final Final commit: Book Review App

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ sequelize
     return sequelize.sync({ force: false });
   })
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
       console.log(`The Database is running on: http://localhost:${PORT}/api`);
     });
